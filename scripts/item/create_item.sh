14ca6640baf742eb839f72491b2b5169eedee139
Dumplingpuff <jason.liang2012@gmail.com>
2016-04-03 15:08:52 -0400

    Able to create items

--- scripts/item/create_item.sh
+++ scripts/item/create_item.sh
@@ -1,5 +1,5 @@
 curl --include --request POST http://localhost:3000/items \
-  --header "Authorization: Token token=BAhJIiViNDMwZDk4NzU5MjA2ZGFjMzA0ZGU0OGUyMWYwYmIyMwY6BkVG--610dfe621fe003aefc7b48f687b7e0f244dc68e6" \
+  --header "Authorization: Token token=BAhJIiUzZjg1YzU1MjFiMDNhZWYyMzRhNjM4NTQ3MmQ2OTQ3OAY6BkVG--68aa09bd61303ae1ddb47d429cc882dd75f386ec" \
   --header "Content-Type: application/json" \
   --data '{
       "item": {
