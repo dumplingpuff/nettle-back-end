f51910f48ffd02df4b6014f1da4a93b6ce86d2a7
Dumplingpuff <jason.liang2012@gmail.com>
2016-04-03 15:03:45 -0400

    prepare item script files for testing.

--- scripts/item/create_item.sh
+++ scripts/item/create_item.sh
@@ -1,9 +1,8 @@
-curl --include --request POST http://localhost:3000/trips \
+curl --include --request POST http://localhost:3000/items \
   --header "Authorization: Token token=BAhJIiViNDMwZDk4NzU5MjA2ZGFjMzA0ZGU0OGUyMWYwYmIyMwY6BkVG--610dfe621fe003aefc7b48f687b7e0f244dc68e6" \
   --header "Content-Type: application/json" \
   --data '{
-      "trip": {
-        "title": "Saco",
-        "location": "New Hampshire"
+      "item": {
+        "content": "Pack your boxers."
       }
   }'
