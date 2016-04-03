1e1943b8639c246b956004c9a023bd1f2814ed7f
Dumplingpuff <jason.liang2012@gmail.com>
2016-04-03 14:08:08 -0400

    wrote index and create action untested

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,30 @@
 class ItemsController < ApplicationController
+before_filter :find_item, only: [:show, :update, :destroy]
+before_filter :item_params, only: [:create, :update]
+skip_before_action :authenticate, only: [:index, :show]
+
 def index
   render json: Item.all
+end
+
+def create
+  @item = Item.new(item_params)
+
+  if @item.save
+    render json: @item, status: :created
+  else
+    render json: @item.errors, status: :unprocessable_entity
+  end
+end
+
+private
+
+def find_item
+  @item = Item.find(params[:id])
+end
+
+def item_params
+  params.require(:item).permit!
 end
 
 end
