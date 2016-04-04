0ce0b3b57c006d8c745b95f997bec49c2298387b
Dumplingpuff <jason.liang2012@gmail.com>
2016-04-03 20:25:12 -0400

    more testing with create trip script

--- scripts/trip/create_trip.sh
+++ scripts/trip/create_trip.sh
@@ -1,9 +1,9 @@
 curl --include --request POST http://localhost:3000/trips \
-  --header "Authorization: Token token=BAhJIiViNDMwZDk4NzU5MjA2ZGFjMzA0ZGU0OGUyMWYwYmIyMwY6BkVG--610dfe621fe003aefc7b48f687b7e0f244dc68e6" \
+  --header "Authorization: Token token=BAhJIiUwMzRiOWE1NjQ3NzAyYWJhZTUxNmU5MzY4ZjI4MDNhNAY6BkVG--05487801b315cc5791631b8156ab80463bd56756" \
   --header "Content-Type: application/json" \
   --data '{
       "trip": {
-        "title": "Saco",
-        "location": "New Hampshire"
+        "title": "Babe",
+        "location": "New Vermont"
       }
   }'
