843a94bde27fe45b1f4df887e4bb3b5973bf4a48
Dumplingpuff <jason.liang2012@gmail.com>
2016-04-05 18:52:11 -0400

    create trip testing

--- scripts/trip/create_trip.sh
+++ scripts/trip/create_trip.sh
@@ -1,9 +1,9 @@
 curl --include --request POST http://localhost:3000/trips \
-  --header "Authorization: Token token=BAhJIiUyYjQ2NjI0YmY3MGQxZWNiNWM2ZjVkZmM1N2QwY2FkZAY6BkVG--7666b3b8fa4d988bdbde15523fdc2bc4b558512b" \
+  --header "Authorization: Token token=BAhJIiVmNWQ1NGY4ODE5NTMyOTMzMjZkNWI4YWNmZDM3YmI0YQY6BkVG--4aab803ea555e35476f22d71e1018e99790d2b37" \
   --header "Content-Type: application/json" \
   --data '{
       "trip": {
-        "title": "Bombb",
+        "title": "Boob bomb",
         "location": "New Vermont"
       }
   }'
