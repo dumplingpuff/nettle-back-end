f7fe0d8e9aaa2c443ea0e34b8feadf62b45909bb
Dumplingpuff <jason.liang2012@gmail.com>
2016-04-05 12:43:14 -0400

    add comments to trip controller and serializer

--- scripts/trip/create_trip.sh
+++ scripts/trip/create_trip.sh
@@ -1,9 +1,9 @@
 curl --include --request POST http://localhost:3000/trips \
-  --header "Authorization: Token token=BAhJIiUxYWZhNDE5YmJlZWJlZTY1YTZkODBjOTNlOTQxMGQ5MwY6BkVG--3fa68e21718666f1d4043e61d8e4d98f690e27d0" \
+  --header "Authorization: Token token=BAhJIiUyYjQ2NjI0YmY3MGQxZWNiNWM2ZjVkZmM1N2QwY2FkZAY6BkVG--7666b3b8fa4d988bdbde15523fdc2bc4b558512b" \
   --header "Content-Type: application/json" \
   --data '{
       "trip": {
-        "title": "Daleeekkksss",
+        "title": "Bombb",
         "location": "New Vermont"
       }
   }'
