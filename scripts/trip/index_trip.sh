2d35dd10fa3b24f6878234fdcac2f14f1a8caceb
Dumplingpuff <jason.liang2012@gmail.com>
2016-04-06 14:47:53 -0400

    index shows trips for current user only

--- scripts/trip/index_trip.sh
+++ scripts/trip/index_trip.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 curl --include --request GET http://localhost:3000/trips \
+  --header "Authorization: Token token=BAhJIiU3YjhjNjUwMDJmMGI0YzlhZjUwMTM1ZGQ3NmViMWJjMQY6BkVG--9bdd971e50f31d1beb3ffa86b2f1a3b0b3e03857" \ 
